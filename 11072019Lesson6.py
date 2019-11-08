eac37d86bbedeca0bc56c601f4be622a4c87fe93
xiaoni0601 <53635574+xiaoni0601@users.noreply.github.com>
2019-11-08 10:18:54 +0800

    PartLesson6

---
+++ 11072019Lesson6.py
@@ -0,0 +1,146 @@
+# ----------------
+# User Instructions
+#
+# Modify the timedcalls(n, fn, *args) function so that it calls 
+# fn(*args) repeatedly. It should call fn n times if n is an integer
+# and up to n seconds if n is a floating point number.
+
+import itertools
+
+def imright(h1, h2):
+    "House h1 is immediately right of h2 if h1-h2 == 1."
+    return h1-h2 == 1
+
+def nextto(h1, h2):
+    "Two houses are next to each other if they differ by 1."
+    return abs(h1-h2) == 1
+
+def zebra_puzzle():
+    "Return a tuple (WATER, ZEBRA indicating their house numbers."
+    houses = first, _, middle, _, _ = [1, 2, 3, 4, 5]
+    orderings = list(itertools.permutations(houses)) # 1
+    return next((WATER, ZEBRA)
+                for (red, green, ivory, yellow, blue) in orderings
+                if imright(green, ivory)
+                for (Englishman, Spaniard, Ukranian, Japanese, Norwegian) in orderings
+                if Englishman is red
+                if Norwegian is first
+                if nextto(Norwegian, blue)
+                for (coffee, tea, milk, oj, WATER) in orderings
+                if coffee is green
+                if Ukranian is tea
+                if milk is middle
+                for (OldGold, Kools, Chesterfields, LuckyStrike, Parliaments) in orderings
+                if Kools is yellow
+                if LuckyStrike is oj
+                if Japanese is Parliaments
+                for (dog, snails, fox, horse, ZEBRA) in orderings
+                if Spaniard is dog
+                if OldGold is snails
+                if nextto(Chesterfields, fox)
+                if nextto(Kools, horse)
+                )
+
+import time
+
+def timedcall(fn, *args):
+    "Call function with args; return the time in seconds and result."
+    t0 = time.clock()
+    result = fn(*args)
+    t1 = time.clock()
+    return t1-t0, result
+
+def average(numbers):
+    "Return the average (arithmetic mean) of a sequence of numbers."
+    return sum(numbers) / float(len(numbers)) 
+
+def timedcalls(n, fn, *args):
+    """Call fn(*args) repeatedly: n times if n is an int, or up to
+    n seconds if n is a float; return the min, avg, and max time"""
+    # Your code here.
+    
+    if isinstance(n, int):
+        times = [timedcall(fn, *args)[0] for _ in range(n)]
+    else:
+        times = []
+        while sum(times) < n:
+            times.append(timedcall(fn, *args)[0])
+    return min(times), average(times), max(times)
+
+
+## design program需要考虑以下几点：
+#1. orrect，是否争取
+#2. efficient,运行效率是否合适，太长不可取
+#3. debugging process
+
+
+## ------------
+# User Instructions
+#
+# Define a function, all_ints(), that generates the 
+# integers in the order 0, +1, -1, +2, -2, ...
+
+def ints(start, end = None):
+    i = start
+    while i <= end or end is None:
+        yield i
+        i = i + 1
+    
+
+def all_ints():
+    "Generate integers in the order 0, +1, -1, +2, -2, +3, -3, ..."
+    # Your code here.
+    yield 0
+    for i in ints(1):
+        yield +i
+        yield -i
+
+#如果不想用for_loop:
+# yield 0
+# i = 1
+# while True:
+#   yield +i
+#   yield -i
+#   i = i + 1
+
+#summary steps for solution:
+#1. make a concept inventory
+#2. refine the ideas
+#3. choose the simplest implementations
+#4. do and run a back of an envelop calculation 
+#5. refine the code
+#6. tools
+#7. clean and clever
+
+
+# another puzzle:
+# -------------
+# -------------
+# User Instructions
+#
+# Complete the fill_in(formula) function by adding your code to
+# the two places marked with ?????. 
+
+import string, re, itertools
+
+def solve(formula):
+    """Given a formula like 'ODD + ODD == EVEN', fill in digits to solve it.
+    Input formula is a string; output is a digit-filled-in string or None."""
+    for f in fill_in(formula):
+        if valid(f):
+            return f
+    
+def fill_in(formula):
+    "Generate all possible fillings-in of letters in formula with digits."
+    letters = ''.join(set(re.findall('[A-Z]', formula))) #should be a string
+    for digits in itertools.permutations('1234567890', len(letters)):
+        table = string.maketrans(letters, ''.join(digits))
+        yield formula.translate(table)
+    
+def valid(f):
+    """Formula f is valid if and only if it has no 
+    numbers with leading zero, and evals true."""
+    try: 
+        return not re.search(r'\b0[0-9]', f) and eval(f) is True
+    except ArithmeticError:
+        return False
