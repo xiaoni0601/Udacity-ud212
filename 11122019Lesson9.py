275969016bbf2177c666a2a912f0b408cc9976d9
xiaoni0601 <53635574+xiaoni0601@users.noreply.github.com>
2019-11-12 18:32:50 +0800

    PartLesson9

---
+++ 11122019Lesson9.py
@@ -0,0 +1,305 @@
+###Regular Expression Review
+
+##Python Language:
+## statements
+## expressions
+#what is a language?---is a set of strings
+#what is a grammar?--description of language
+#difference between a compiler and an interpreter
+# use a language as a design tool
+# 
+# a language for regular expression 
+
+# API: application programming interface
+
+
+
+#----------------
+# User Instructions
+#
+# The function, matchset, takes a pattern and a text as input
+# and returns a set of remainders. For example, if matchset 
+# were called with the pattern star(lit(a)) and the text 
+# 'aaab', matchset would return a set with elements 
+# {'aaab', 'aab', 'ab', 'b'}, since a* can consume one, two
+# or all three of the a's in the text.
+#
+# Your job is to complete this function by filling in the 
+# 'dot' and 'oneof' operators to return the correct set of 
+# remainders.
+#
+# dot:   matches any character.
+# oneof: matches any of the characters in the string it is 
+#        called with. oneof('abc') will match a or b or c.
+
+def matchset(pattern, text):
+    "Match pattern at start of text; return a set of remainders of text."
+    op, x, y = components(pattern)
+    if 'lit' == op:
+        return set([text[len(x):]]) if text.startswith(x) else null
+    elif 'seq' == op:
+        return set(t2 for t1 in matchset(x, text) for t2 in matchset(y, t1))
+    elif 'alt' == op:
+        return matchset(x, text) | matchset(y, text)
+    elif 'dot' == op:
+        return set([text[1:]]) if text else null  # your code here
+    elif 'oneof' == op:
+        return set([text[1:]]) if any(text.startswith(c) for a in x ) else null    # your code here
+    elif 'eol' == op:
+        return set(['']) if text == '' else null
+    elif 'star' == op:
+        return (set([text]) |
+                set(t2 for t1 in matchset(x, text)
+                    for t2 in matchset(pattern, t1) if t1 != text))
+    else:
+        raise ValueError('unknown pattern: %s' % pattern)
+        
+null = frozenset()
+
+def components(pattern):
+    "Return the op, x, and y arguments; x and y are None if missing."
+    x = pattern[1] if len(pattern) > 1 else None
+    y = pattern[2] if len(pattern) > 2 else None
+    return pattern[0], x, y
+   
+def test():
+    assert matchset(('lit', 'abc'), 'abcdef')            == set(['def'])
+    assert matchset(('seq', ('lit', 'hi '),
+                     ('lit', 'there ')), 
+                   'hi there nice to meet you')          == set(['nice to meet you'])
+    assert matchset(('alt', ('lit', 'dog'), 
+                    ('lit', 'cat')), 'dog and cat')      == set([' and cat'])
+    assert matchset(('dot',), 'am i missing something?') == set(['m i missing something?'])
+    assert matchset(('oneof', 'a'), 'aabc123')           == set(['abc123'])
+    assert matchset(('eol',),'')                         == set([''])
+    assert matchset(('eol',),'not end of line')          == frozenset([])
+    assert matchset(('star', ('lit', 'hey')), 'heyhey!') == set(['!', 'heyhey!', 'hey!'])
+    
+    return 'tests pass'
+
+print test()
+
+    
+#---------------
+# User Instructions
+#
+# Fill out the API by completing the entries for alt, 
+# star, plus, and eol.
+
+
+def lit(string):  return ('lit', string)
+def seq(x, y):    return ('seq', x, y)
+def alt(x, y):    return ('alt', x, y)  # ??
+def star(x):      return ('star', x)    # ??
+def plus(x):      return seq(x, star(x))  # ??
+def opt(x):       return alt(lit(''), x) #opt(x) means that x is optional
+def oneof(chars): return ('oneof', tuple(chars))
+dot = ('dot',)
+eol = ('eol',) #??
+
+def test():
+    assert lit('abc')         == ('lit', 'abc')
+    assert seq(('lit', 'a'), 
+               ('lit', 'b'))  == ('seq', ('lit', 'a'), ('lit', 'b'))
+    assert alt(('lit', 'a'), 
+               ('lit', 'b'))  == ('alt', ('lit', 'a'), ('lit', 'b'))
+    assert star(('lit', 'a')) == ('star', ('lit', 'a'))
+    assert plus(('lit', 'c')) == ('seq', ('lit', 'c'), 
+                                  ('star', ('lit', 'c')))
+    assert opt(('lit', 'x'))  == ('alt', ('lit', ''), ('lit', 'x'))
+    assert oneof('abc')       == ('oneof', ('a', 'b', 'c'))
+    return 'tests pass'
+
+print test()
+
+
+
+# in term of API:
+# a function serch(pattern, text)--> string,anywhere in the string/None
+# a function match(pattern, text)--> string, at the start of the string/None
+# creat patterns:
+# lit(s) = literal(string) 
+# alt(x, y) = alternative(pattern(x), pattern(y)) 
+# seq(x, y) = sequence(pattern(x), pattern(y))
+
+
+#---------------
+# User Instructions
+#
+# Complete the search and match functions. Match should
+# match a pattern only at the start of the text. Search
+# should match anywhere in the text.
+
+def search(pattern, text):
+    "Match pattern anywhere in text; return longest earliest match or None."
+    for i in range(len(text)):
+        m = match(pattern, text[i:])
+        if m is not None# your code here
+            return m
+        
+def match(pattern, text):
+    "Match pattern against start of text; return longest match found or None."
+    remainders = matchset(pattern, text)
+    if remainders:
+        shortest = min(remainders, key=len)
+        return text[:len(text)-len(shortest)]# your code here
+    
+def components(pattern):
+    "Return the op, x, and y arguments; x and y are None if missing."
+    x = pattern[1] if len(pattern) > 1 else None
+    y = pattern[2] if len(pattern) > 2 else None
+    return pattern[0], x, y
+
+def matchset(pattern, text):
+    "Match pattern at start of text; return a set of remainders of text."
+    op, x, y = components(pattern)
+    if 'lit' == op:
+        return set([text[len(x):]]) if text.startswith(x) else null
+    elif 'seq' == op:
+        return set(t2 for t1 in matchset(x, text) for t2 in matchset(y, t1))
+    elif 'alt' == op:
+        return matchset(x, text) | matchset(y, text)
+    elif 'dot' == op:
+        return set([text[1:]]) if text else null
+    elif 'oneof' == op:
+        return set([text[1:]]) if text.startswith(x) else null
+    elif 'eol' == op:
+        return set(['']) if text == '' else null
+    elif 'star' == op:
+        return (set([text]) |
+                set(t2 for t1 in matchset(x, text)
+                    for t2 in matchset(pattern, t1) if t1 != text))
+    else:
+        raise ValueError('unknown pattern: %s' % pattern)
+    
+null = frozenset()
+
+def lit(string):  return ('lit', string)
+def seq(x, y):    return ('seq', x, y)
+def alt(x, y):    return ('alt', x, y)
+def star(x):      return ('star', x)
+def plus(x):      return seq(x, star(x))
+def opt(x):       return alt(lit(''), x)
+def oneof(chars): return ('oneof', tuple(chars))
+dot = ('dot',)
+eol = ('eol',)
+
+def test():
+    assert match(('star', ('lit', 'a')),'aaabcd') == 'aaa'
+    assert match(('alt', ('lit', 'b'), ('lit', 'c')), 'ab') == None
+    assert match(('alt', ('lit', 'b'), ('lit', 'a')), 'ab') == 'a'
+    assert search(('alt', ('lit', 'b'), ('lit', 'c')), 'ab') == 'b'
+    return 'tests pass'
+
+print test()
+
+
+##Interpreter or Compiler,解释器 or 编译器 ？
+# Interpreter: matchset(pattern, text)
+# Compiler: do all works at once
+
+
+
+#----------------
+# User Instructions
+#
+# Write the compiler for alt(x, y) in the same way that we 
+# wrote the compiler for lit(s) and seq(x, y). 
+
+'''
+def matchset(pattern, text):
+    op, x, y = components(pattern)
+    if 'lit' == op:
+        return set([text[len(x):]]) if text.startswith(x) else null
+    elif 'seq' == op:
+        return set(t2 for t1 in matchset(x, text) for t2 in matchset(y, t1))
+    elif 'alt' == op:
+        return matchset(x, text) | matchset(y, text)
+'''
+
+def lit(s): return lambda text: set([text[len(s):]]) if text.startswith(s) else null
+
+def seq(x, y): return lambda text: set().union(*map(y, x(text)))
+
+def alt(x, y): return lambda text: x(text) | y(text)# your code here
+        
+null = frozenset([])
+
+def test():
+    g = alt(lit('a'), lit('b'))
+    assert g('abc') == set(['bc'])
+    return 'test passes'
+
+print test()
+
+
+
+# --------------
+# User Instructions
+#
+# Fill out the function match(pattern, text), so that 
+# remainders is properly assigned. 
+
+def match(pattern, text):
+    "Match pattern against start of text; return longest match found or None."
+    remainders = pattern(text)# your code here.
+    if remainders:
+        shortest = min(remainders, key=len)
+        return text[:len(text)-len(shortest)]
+    
+def lit(s): return lambda t: set([t[len(s):]]) if t.startswith(s) else null
+def seq(x, y): return lambda t: set().union(*map(y, x(t)))
+def alt(x, y): return lambda t: x(t) | y(t)
+def oneof(chars): return lambda t: set([t[1:]]) if (t and t[0] in chars) else null
+dot = lambda t: set([t[1:]]) if t else null
+eol = lambda t: set(['']) if t == '' else null
+def star(x): return lambda t: (set([t]) | 
+                               set(t2 for t1 in x(t) if t1 != t
+                                   for t2 in star(x)(t1)))
+
+null = frozenset([])
+
+def test():
+    assert match(star(lit('a')), 'aaaaabbbaa') == 'aaaaa'
+    assert match(lit('hello'), 'hello how are you?') == 'hello'
+    assert match(lit('x'), 'hello how are you?') == None
+    assert match(oneof('xyz'), 'x**2 + y**2 = r**2') == 'x'
+    assert match(oneof('xyz'), '   x is here!') == None
+    return 'tests pass'
+
+
+
+# --------------
+# User Instructions
+#
+# Complete the code for the compiler by completing the constructor
+# for the patterns alt(x, y) and oneof(chars). 
+
+def lit(s):         return lambda Ns: set([s]) if len(s) in Ns else null
+def alt(x, y):      return lambda Ns: x(Ns) | y(Ns)# your code here
+def star(x):        return lambda Ns: opt(plus(x))(Ns)
+def plus(x):        return lambda Ns: genseq(x, star(x), Ns, startx=1) #Tricky
+def oneof(chars):   return lambda Ns: set(chars) if 1 in Ns else null# your code here
+def seq(x, y):      return lambda Ns: genseq(x, y, Ns)
+def opt(x):         return alt(epsilon, x)
+dot = oneof('?')    # You could expand the alphabet to more chars.
+epsilon = lit('')   # The pattern that matches the empty string.
+
+null = frozenset([])
+
+def test():
+    
+    f = lit('hello')
+    assert f(set([1, 2, 3, 4, 5])) == set(['hello'])
+    assert f(set([1, 2, 3, 4]))    == null 
+    
+    g = alt(lit('hi'), lit('bye'))
+    assert g(set([1, 2, 3, 4, 5, 6])) == set(['bye', 'hi'])
+    assert g(set([1, 3, 5])) == set(['bye'])
+    
+    h = oneof('theseletters')
+    assert h(set([1, 2, 3])) == set(['t', 'h', 'e', 's', 'l', 'r'])
+    assert h(set([2, 3, 4])) == null
+    
+    return 'tests pass'
+print test()
